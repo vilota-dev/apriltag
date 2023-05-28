7428765b9ca4ad4fcbf8bfd6b3b800a2cb5b6290
chengguizi <cgz_05@sina.com>
2023-05-28 12:02:40 +0800

    add new codes tag25h7, compatible with apriltag2

---
+++ tag25h7.h
@@ -0,0 +1,17 @@
+#ifndef _TAG25H7
+#define _TAG25H7
+
+#include "apriltag.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+apriltag_family_t *tag25h7_create();
+void tag25h7_destroy(apriltag_family_t *tf);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
